fd632d6e7401aa4f4bde2bf3444687335e5b84f7
Arunakiri <arunakiri.v@gmail.com>
2020-11-11 16:41:07 +0530

    Comments added

--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -1,46 +1,23 @@
-# Solving Sudoku using Backtracking Algorithm
-value = "0 7 0 0 0 0 0 9 1, " \
-        "0 5 0 3 0 0 0 2 0, " \
-        "0 0 0 8 0 0 0 4 5, " \
-        "0 8 5 0 3 0 0 0 0, " \
-        "0 3 0 0 0 8 0 6 2, " \
-        "0 1 9 0 6 0 0 0 0, " \
-        "0 0 0 4 0 0 0 0 0, " \
-        "0 4 2 0 1 5 0 8 7, " \
-        "0 0 0 6 0 3 2 5 0"
+# Solving Sudoku using Backtracking(Recursive) Algorithm
+def solve(board):
+    # print(board)  # Shows what happens each step
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
 
-board = [list(map(int, each.strip().split())) for each in value.split(',')]
+    for num in range(1, 10):
+        if valid(board, num, (row,col)):
+            board[row][col] = num
+            if solve(board):
+                return True
+            board[row][col] = 0
 
-
-def style(arr):
-    temp = ''
-    for i in range(len(arr)):
-        if (i + 1) % 3 == 0 and i + 1 != 9:
-            temp += str(arr[i])+' | '
-        else:
-            temp += str(arr[i])+' '
-
-    return temp
+    return False
 
 
-def print_board(board):
-    for i in range(len(board)):
-        if i == 9:
-            break
-        if (i + 1) % 3 == 0 and i + 1 != 9:
-            print(style(board[i])+'\n'+'- - - - - - - - - - -')
-        else:
-            print(style(board[i]))
-
-
-def find_empty(board):
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 0:
-                return i, j   # position
-    return None
-
-
+# Check for validity of the number on a position in the board
 def valid(board, num, pos):
     # check validity along row
     for i in range(len(board)):
@@ -64,26 +41,53 @@ def valid(board, num, pos):
     return True
 
 
-def solve(board):
-    find = find_empty(board)
-    if not find:
-        return True
-    else:
-        row, col = find
+# Adding Formats
+def style(arr):
+    temp = ''
+    for i in range(len(arr)):
+        if (i + 1) % 3 == 0 and i + 1 != 9:
+            temp += str(arr[i])+' | '
+        else:
+            temp += str(arr[i])+' '
 
-    for num in range(1, 10):
-        if valid(board, num, (row,col)):
-            board[row][col] = num
-            if solve(board):
-                return True
-            board[row][col] = 0
-
-    return False
+    return temp
 
 
-print_board(board)
+# Print the Board
+def print_board(board):
+    for i in range(len(board)):
+        if i == 9:
+            break
+        if (i + 1) % 3 == 0 and i + 1 != 9:
+            print(style(board[i])+'\n'+'- - - - - - - - - - -')
+        else:
+            print(style(board[i]))
+
+
+# Find the empty position on the board
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 0:
+                return i, j   # position
+    return None
+
+
+value = "7 8 0 4 0 0 1 2 0, " \
+        "6 0 0 0 7 5 0 0 9, " \
+        "0 0 0 6 0 1 0 7 8, " \
+        "0 0 7 0 4 0 2 6 0, " \
+        "0 0 1 0 5 0 9 3 0, " \
+        "9 0 4 0 6 0 0 0 5, " \
+        "0 7 0 3 0 0 0 1 2, " \
+        "1 2 0 0 0 7 4 0 0, " \
+        "0 4 9 2 0 6 0 0 7"
+
+board = [list(map(int, each.strip().split())) for each in value.split(',')]
+
+# print_board(board)
 solve(board)
-print('---------------------')
+# print('---------------------')
 print_board(board)
 
 
